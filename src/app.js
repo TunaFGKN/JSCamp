dff1c6dca29a3666acf7387082bf374053e37cb6
TunaFigankaplan <swaxztn@gmail.com>
2021-10-19 00:02:12 +0300

    Starter Kit Entegrasyonu

---
+++ src/app.js
@@ -0,0 +1,8 @@
+console.log("Hello World!")
+// var global değişken gibi let özel değişken gibidir.
+// var'ı dışarda tanımlayıp aynı isimle başka bir blokta başka bir şey tanımlarsan önceki tanımlanan da değişir
+// let'i aynı isimle başka bir blokta kullanırsan dışarıda ayrı blokta ayrı değer alır
+// JS type safe değildir
+// const read onlydir bir kere tanımladıktan sonra onu başka bir yerden değiştiremezsin
+// array ["kredi 1","kredi 2","kredi 3"] --- for
+
