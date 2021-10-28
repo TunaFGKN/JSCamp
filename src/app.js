16b5ff1e4584401d3a86f5c79d55d9a23b59a74d
TunaFigankaplan <swaxztn@gmail.com>
2021-10-28 20:01:55 +0300

    rest spread destruct prototype map filter reduce

--- src/app.js
+++ src/app.js
@@ -1,9 +1,58 @@
 console.log("Hello World!")
-// var global değişken gibi let özel değişken gibidir.
+// + var global değişken gibi let özel değişken gibidir.
 // var'ı dışarda tanımlayıp aynı isimle başka bir blokta başka bir şey tanımlarsan önceki tanımlanan da değişir
-// let'i aynı isimle başka bir blokta kullanırsan dışarıda ayrı blokta ayrı değer alır
+// - let'i aynı isimle başka bir blokta kullanırsan dışarıda ayrı blokta ayrı değer alır
 // JS type safe değildir
 // const read onlydir bir kere tanımladıktan sonra onu başka bir yerden değiştiremezsin
 // array ["kredi 1","kredi 2","kredi 3"] --- for
 
-// -
+//let student = {id:1, name="Tuna"}
+
+//function save(stundent) {
+//    console.log("student")
+//}
+
+//save() parametre gönderilme ama hata vermez "undefined"
+
+// 9-1 -> bu iki studentin birbiriyle alakası yok
+
+//rest
+let showProducts = function (id,...products) { //array
+    console.log(id)
+    console.log(products)
+}
+
+console.log(typeof showProducts)
+showProducts(10,"Elma","Armut","Karpuz")
+
+
+//spread
+let points = [1,2,3,4,50,5,60,14]
+console.log(points)
+console.log(...points)
+console.log(Math.max(...points))
+console.log(..."ABC","D",..."EFG","H","XYZ")
+
+//Destructuring - önemli - elinizdeki bir arrayi ayrı ayrı değişkenlere atamak
+let populations = [10000,20000,30000,[40000,50000]] //normal array
+let [small,medium,high,[veryHigh,max]] = populations // small high mediuma populationsu aktar
+console.log(small)
+console.log(high)
+console.log(medium)
+console.log(veryHigh)
+console.log(max)
+
+function someFunction([small1],number) {
+    console.log(small1)
+}
+
+someFunction(populations)
+
+let category = {id:1,name:"Beverages"}
+console.log(category.id)
+console.log(category["name"])
+
+let {id,name} = category //object destruct
+console.log(id)
+console.log(name)
+
---
+++ src/oop/index.js
@@ -0,0 +1,37 @@
+class Customer{
+    constructor(id,customerNumber){
+        this.id = id // this demek instance'a karşılık gelir (7.satır)
+        this.customerNumber = customerNumber
+    }
+}
+
+let customer = new Customer(1,"12345")
+//prototyping
+customer.name = "Murat"
+console.log(customer.name)
+
+Customer.bisey = "Bişey"
+console.log(Customer.bisey)
+
+console.log(customer.customerNumber)
+
+class IndiCustomer extends Customer{
+    constructor(firstName,id,customerNumber){
+        super(id,customerNumber) //verileri base classa gönderir
+        this.firstName = firstName
+    }
+}
+
+class CoopCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+        super(id,customerNumber) //verileri base classa gönderir
+        this.companyName = companyName
+    }
+}
+
+//şimdi benim bir tane arrayim var arrayin içinde de objeler var 
+//arrayismi.map(takmaisim => {takmaisim.objeninpropu} )
+
+//filter yepyeni bir array çıkarır bizim için
+
+//let toplam = products.reduce((acc,product)=>acc+product.unitPrice,0)
